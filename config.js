b0d073a16b02dbcc60f8bfa0c3a59e551199b37d
skul-hub <yoelafrido42@gmail.com>
2025-09-09 23:14:56 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
-const supabaseUrl = 'https://rzvzhnfrogokupuzjipn.supabase.co'; 
-const supabaseKey = 'YOUR_SUPABASE_ANON_KEY'; // ganti dengan anon key dari Supabase
+const supabaseUrl = 'https://yegkaqzynckagopjinnh.supabase.co'; 
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InllZ2thcXp5bmNrYWdvcGppbm5oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc0MjY5OTQsImV4cCI6MjA3MzAwMjk5NH0.nuxlqVjSJdJGDjrgM2tPmJw-h3i5f4M1aXk_eYej5RQ'; // ganti dengan anon key dari Supabase
 
 const { createClient } = supabase;
 window.supabase = createClient(supabaseUrl, supabaseKey);
