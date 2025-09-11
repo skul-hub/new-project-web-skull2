12fb58c96654c84e84e54c11bbb991c9ca660ce2
skul-hub <yoelafrido42@gmail.com>
2025-09-12 00:25:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
-const supabaseUrl = 'https://qnwmhaiulcvamdfhpyev.supabase.co'; // GANTI DENGAN URL SUPABASE ANDA
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFud21oYWl1bGN2YW1kZmhweWV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc1MjE3MzMsImV4cCI6MjA3MzA5NzczM30.Nx8mmWebHIQKeJJQ0WHPsrrSrYr97YSMPGYkOGazKPQ'; // GANTI DENGAN ANON KEY SUPABASE ANDA
+const supabaseUrl = 'https://nnycywdkbtfludsuuvys.supabase.co'; // GANTI DENGAN URL SUPABASE ANDA
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ueWN5d2RrYnRmbHVkc3V1dnlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc2MTAyMTksImV4cCI6MjA3MzE4NjIxOX0.Hfp-ahW4S29SuORA3803ZCIivrwuquFgoSaTHUkALJU'; // GANTI DENGAN ANON KEY SUPABASE ANDA
 
 const { createClient } = supabase;
 window.supabase = createClient(supabaseUrl, supabaseKey);
